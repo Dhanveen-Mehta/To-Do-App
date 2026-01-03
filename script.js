64ad0389cf218b30b26fc7df512d5225803ef3ee
Dhanveen Mehta <dhanveenmehta2007@gmail.com>
2026-01-03 12:53:13 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 let addTask = document.querySelector("#add-task")
 let addInput = document.querySelector("#input-write-task");
 let taskItems = document.querySelector("#task-items");
-
+let taskNumber = 0;
 
 
 addTask.addEventListener("click",function(){
-     li = document.createElement("li");
-    console.log(addInput.value)
-    li.innerText = addInput.value;
-    taskItems.appendChild(li)
-    addInput.value = '';
+    if (addInput.value == ''){
+        console.error("Enter a task first")
+    }else{
+          //  taskNumber = taskNumber+1;
+            //console.log(taskNumber)
+           let deleteBtn = document.createElement("button")
+            deleteBtn.innerText = "Delete Task"
+           let li = document.createElement("li");
+            li.setAttribute("id",taskNumber);
+            console.log(addInput.value)
+            li.innerText = addInput.value;
+            taskItems.appendChild(li)
+            li.appendChild(deleteBtn)
+            addInput.value = '';
+        
+        
+            deleteBtn.addEventListener("click",function(){
+             //console.log(li.getAttribute("id"));
+             
+                li.remove()
+
+
+
+
+
+
+
+
+            })
+        
 
 
 
@@ -27,13 +52,8 @@ addTask.addEventListener("click",function(){
 
 
 
-
-
-
-
-    // addInput = '';
-   
-
+        }
 
 
 })
+
